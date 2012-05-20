ee979dca8332a10a6b8bbe93b8228043471e2af3
Yurii Rashkovskii <yrashk@gmail.com>
2012-05-20 16:04:43 -0700

    Adds Type validator

--- lib/validatex.ex
+++ lib/validatex.ex
@@ -26,6 +26,9 @@ defrecord Format,
 defrecord Length,
           is: nil
 
+defrecord Type,
+          is: nil
+
 defprotocol Validate do
   @only [Record, Any]
   def valid?(validator, data)
@@ -124,6 +127,25 @@ defimpl Validate, for: Length do
       Validatex.Validate.valid?(validator)
   end
 
+end
+
+defimpl Validate, for: Type do
+    refer Type, as: T
+    def valid?(T[ is: :nil], nil), do: true
+    def valid?(T[ is: :number ], a) when is_number(a), do: true
+    def valid?(T[ is: :integer ], a) when is_integer(a), do: true
+    def valid?(T[ is: :float ], a) when is_float(a), do: true
+    def valid?(T[ is: :boolean ], a) when is_boolean(a), do: true
+    def valid?(T[ is: :atom ], a) when is_atom(a), do: true
+    def valid?(T[ is: :binary ], a) when is_binary(a), do: true
+    def valid?(T[ is: :string ], a) when is_binary(a), do: true
+    def valid?(T[ is: :reference ], a) when is_reference(a), do: true
+    def valid?(T[ is: :function ], a) when is_function(a), do: true
+    def valid?(T[ is: :port ], a) when is_port(a), do: true
+    def valid?(T[ is: :pid ], a) when is_pid(a), do: true
+    def valid?(T[ is: :tuple ], a) when is_tuple(a), do: true
+    def valid?(T[ is: :list ], a) when is_list(a), do: true
+    def valid?(T[], _), do: false
 end
 
 defimpl Validate, for: Any do
---
+++ test/type_test.exs
@@ -0,0 +1,33 @@
+Code.require_file "../test_helper", __FILE__
+
+defmodule Validatex.TypeTest do
+  use ExUnit.Case
+  refer Validatex.Validate, as: V
+  refer Validatex.Type, as: T
+
+  test :integer do
+    assert V.valid?(T.new(is: :number),1)
+    assert V.valid?(T.new(is: :integer),1)
+  end
+
+  test :float do 
+    assert V.valid?(T.new(is: :number),1.1)
+    assert V.valid?(T.new(is: :float),1.1)
+  end
+
+  test :atom, do: assert V.valid?(T.new(is: :atom), :atom)
+  test :binary, do: assert V.valid?(T.new(is: :binary), <<>>)
+  test :string, do: assert V.valid?(T.new(is: :string), "")
+  test :ref, do: assert V.valid?(T.new(is: :reference), make_ref())
+  test :fun, do: assert V.valid?(T.new(is: :function), fn () -> :ok end)
+  test :port, do: assert V.valid?(T.new(is: :port), hd(:erlang.ports))
+  test :pid, do: assert V.valid?(T.new(is: :pid), Process.self)
+  test :tuple, do: assert V.valid?(T.new(is: :tuple), {})
+  test :list, do: assert V.valid?(T.new(is: :list), [])
+  test :boolean do
+    assert V.valid?(T.new(is: :boolean), true)
+    assert V.valid?(T.new(is: :boolean), false)
+  end
+  test :nil, do: assert V.valid?(T.new(is: :nil), nil)
+
+end
