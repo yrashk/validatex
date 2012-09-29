c40a64f282c7d3f06371c6af2a87bf77dfa6c1c2
Yurii Rashkovskii <yrashk@spawngrid.com>
2012-09-28 21:49:48 -0700

    Add expm package.exs

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "validatex", description: "Validatex is a simple Elixir library for validating (input) data",
+                 version: "0.0.1", keywords: ["string","input","data","validate"], 
+                 maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/validatex", tag: "0.0.1"]])
