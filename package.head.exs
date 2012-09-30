fa2d06a508cb34ac4731f3c25637c83207472c3e
Yurii Rashkovskii <yrashk@gmail.com>
2012-09-30 01:34:49 -0700

    Add package file for the head version

---
+++ package.head.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "validatex", description: "Validatex is a simple Elixir library for validating (input) data",
+                 version: :head, keywords: ["string","input","data","validate"], 
+                 maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/validatex"]])
